8743010c9fb06ac378d3dbcf1c1b37d30ffd0a2b
Yorli <yorli0114@gmail.com>
2020-11-02 14:24:59 -0500

    began defining user route

---
+++ controllers/userController.js
@@ -0,0 +1,11 @@
+const userController = {
+    signIn(req,res) {
+        
+
+    },
+    register(req,res) {
+
+    }
+};
+
+export default userController;
---
+++ services/userServices/userService.js
@@ -0,0 +1,10 @@
+const userService = {
+    signInUser(email,password){
+
+    },
+    registerUser(email,password,firstName,lastName){
+
+    }
+}
+
+export default userService;
